18acdbeed075be6317b51d54b8357a6a9b307359
Shivanthi Fernando <wscsfernando@gmail.com>
2024-04-13 22:01:05 +0530

    Created the Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
 import NavBar from "./components/NavBar/NavBar";
+import Hero from "./components/Hero/Hero";
 
 const App = () => {
   return (
     <div>
       <NavBar />
+      <Hero />
     </div>
   );
 };
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -78,7 +78,7 @@ const NavBar = () => {
                   placeholder="Search"
                   className="w-[200px] sm:w-[200px] group-hover:w-[300px] transition-all duration-300 rounded-full border border-gray-300 px-4 py-1 focus:outline-none focus:border-1 focus:border-primary dark:border-gray-500 dark:bg-gray-800"
                 />
-                <IoMdSearch className="text-gray-500 group-hover: text-primary absolute top-1/2 -translate-y-1/2 right-3" />
+                <IoMdSearch className="text-gray-500 group-hover:text-primary absolute top-1/2 -translate-y-1/2 right-3" />
               </div>
             </div>
             {/* Order Buttons */}
