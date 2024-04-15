dff586c3410af6b87a67bab49860f1ba7b3351b2
Shivanthi Fernando <wscsfernando@gmail.com>
2024-04-15 13:00:52 +0530

    Added AOS feature

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,20 @@ import React from "react";
 import NavBar from "./components/NavBar/NavBar";
 import Hero from "./components/Hero/Hero";
 import BestSellers from "./components/BestSellers/BestSellers";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const App = () => {
+  React.useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 800,
+      easing: "ease-in-sine",
+      delay: 100,
+    });
+    AOS.refresh();
+  }, []);
+
   return (
     <div>
       <NavBar />
